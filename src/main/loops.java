8eb5811fb93a8241515c011978798463f3b58775
Aleena Wahid (UST <U60903@ust-global.com>
2019-08-08 11:42:21 +0530

    added src/main

---
+++ src/main/loops.java
@@ -0,0 +1,25 @@
+public class Test {
+
+   public static void main(String args[]) {
+//for loop
+      for(int x = 10; x < 20; x = x + 1) {
+         System.out.print("value of x : " + x );
+         System.out.print("\n");
+		 
+      }
+	  //do while loop
+	  do {
+         System.out.print("value of x : " + x );
+         x++;
+         System.out.print("\n");
+      }while( x < 20 );
+	  
+	  if(a>b)
+	  {
+	System.out.println("a is greater");
+   }
+   else
+   {
+   System.out.println("b is greater");
+   }
+}
